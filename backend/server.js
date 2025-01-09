5ace3accd8979123a5a3e26cd8ff085a09469bd3
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-09 14:52:51 +0000

    product schema

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 const app = express();
+
+app.post("/products", async(req, res)=>{
+  const product = req.body;
+
+  if(!product.name || !product.price || !product.image){
+    return res.status(400).json({success:false, message: "Please provide all fields"})
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success:true, data: newProduct});
+  } catch (error) {
+    console.error("Error is create product ", error.message);
+    res.status(500).json({success:false, message: "server error"})
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("server maybe running");
