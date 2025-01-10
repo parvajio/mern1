e02374f63f906eda1909ad96c6ea8f09ce7359aa
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2025-01-10 12:43:29 +0600

    rearrange with controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,56 +1,14 @@
 import express from "express"
-import Product from "../models/product.model.js";
+import { createProduct, deleteProduct, getProduct, updateProduct } from "../controller/product.control.js";
 
 const router = express.Router();
 
-router.post("/", async(req, res)=>{
-    const product = req.body;
+router.post("/", createProduct)
   
-    if(!product.name || !product.price || !product.image){
-      return res.status(400).json({success:false, message: "Please provide all fields"})
-    }
+  router.get("/", getProduct)
   
-    const newProduct = new Product(product);
+  router.delete('/:id', deleteProduct)
   
-    try {
-      await newProduct.save();
-      res.status(201).json({success:true, data: newProduct});
-    } catch (error) {
-      console.error("Error is create product ", error.message);
-      res.status(500).json({success:false, message: "server error"})
-    }
-  })
-  
-  router.get("/", async(req, res)=>{
-    try{
-      const products = await Product.find({});
-      res.status(200).json({success: true, data: products})
-    }catch(error){
-      res.status(500).json({success: false, message: "server error"})
-    }
-  })
-  
-  router.delete('/:id', async(req, res)=>{
-    const {id} = req.params;
-  
-    try {
-      await Product.findByIdAndDelete(id);
-      res.status(200).json({success: true, message: "successfullhy deleted a product"})
-    } catch (error) {
-      res.status(404).json({success: false, message: "the product not found"})
-    }
-  })
-  
-  router.put('/:id', async(req, res)=>{
-    const {id} = req.params;
-    const product = req.body;
-  
-    try{
-      const updatedProduct = await Product.findByIdAndUpdate(id, product,{new : true});
-      res.status(200).json({success: true, data: updatedProduct})
-    }catch(err){
-      res.status(500).json({success: false, message: "server error"})
-    }
-  })
+  router.put('/:id', updateProduct)
 
 export default router;
