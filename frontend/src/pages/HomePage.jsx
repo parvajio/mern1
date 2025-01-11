3926056f041c9cfc797e73e500ca5d350398a58f
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2025-01-11 21:54:48 +0600

    homePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,29 +1,29 @@
-import { Container, Link, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+// import { Container, Link, Text, VStack } from '@chakra-ui/react'
+// import React from 'react'
 
-const HomePage = () => {
-  return (
-    <Container maxW={'container.xl'} py={12}>
-      <VStack spacing={8}>
-        <Text
-          fontSize={'30'}
-          fontWeight={"bold"}
-          bgGradient='linear(to-l, #7928CA, #FF0080)'
-          bgClip={"text"}
-          textAlign={'center'}
-        >
-          Curent Product
-        </Text>
+// const HomePage = () => {
+//   return (
+//     <Container maxW={'container.xl'} py={12}>
+//       <VStack spacing={8}>
+//         <Text
+//           fontSize={'30'}
+//           fontWeight={"bold"}
+//           bgGradient='linear(to-l, #7928CA, #FF0080)'
+//           bgClip={"text"}
+//           textAlign={'center'}
+//         >
+//           Curent Product
+//         </Text>
 
-        <Text fontSize={'xl'} textColor={'gray.500'}>
-          No Product found!! 
-          <Link href='/create' textColor={'blue.500'}>
-            Create Product
-          </Link>
-        </Text>
-      </VStack>
-    </Container>
-  )
-}
+//         <Text fontSize={'xl'} textColor={'gray.500'}>
+//           No Product found!! 
+//           <Link href='/create' textColor={'blue.500'}>
+//             Create Product
+//           </Link>
+//         </Text>
+//       </VStack>
+//     </Container>
+//   )
+// }
 
-export default HomePage
+// export default HomePage
