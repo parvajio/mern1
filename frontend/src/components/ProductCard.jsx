9e890b773a967f0948dcd6333e04d114d4fd96bf
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2025-01-12 22:47:03 +0600

    product card created and showing products in web

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,17 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
-    <div>ProductCard</div>
+    <Box
+        shadow={'lg'}
+        rounded={"lg"}
+        overflow={"hidden"}
+        transition={'all 0.3s'}
+        _hover={{transform: "translateV(-5px) ", shadow: "xl"}}
+    >
+        {product.name}
+    </Box>
   )
 }
 
