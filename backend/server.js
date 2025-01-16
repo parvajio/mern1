3366a1dfd6d5b8d7c85cb3e308d9486678cea56d
Md. Parvaj Mosharof <mdparvajmosharof17@gmail.com>
2025-01-17 00:52:48 +0600

    production ready

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,29 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 import productRouter from "./routes/product.route.js"
+import path from 'path'
 
 const app = express();
+const port = process.env.PORT
+
+const __dirname = path.resolve()
 
 app.use(express.json()); //allow to accept json data in the req.body
 
 app.use("/api/products", productRouter);
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'frontend', "dist", "index.html"))
+  })
+}
+
 app.get("/", (req, res) => {
   res.send("server maybe running");
 });
 
-const port = process.env.PORT
 
 app.listen(port, () => {
   connectDB()
